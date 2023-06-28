67430ec585c735844caffa425fca5148aeccccac
Raghuram Gudimetla <gudimetla.raghu36@gmail.com>
2023-06-28 16:17:04 +1200

    Update the lambda to upload file to bucket

--- lambdas/functions/data_extraction/lambda_function.py
+++ lambdas/functions/data_extraction/lambda_function.py
@@ -1,8 +1,15 @@
 #!python
 import awswrangler as wr
 import requests
-import logging
+import pandas as pd
 import json
+import boto3
+from datetime import datetime
+import pytz
+import logging
+
+logging.getLogger("snowflake.connector.network").disabled = True
+logging.getLogger().setLevel(logging.INFO)
 
 def read_response_json_data(url: str) -> dict:
     """
@@ -25,6 +32,20 @@ def lambda_handler(event, context):
         context: None
     """
 
-    nzd_currency_api = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/nzd.json"
-    print(f"URL: {nzd_currency_api}")
+    bucket_name = 'ap-southeast-2-886192468297-data-extraction'
+    bucket_filename = datetime.now(pytz.timezone("Pacific/Auckland")).strftime('%Y%m%d') + '-nzd-currency.json'
+    local_file_name = '/tmp/sample.json'
 
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(bucket_name)
+
+    nzd_currency_api = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/nzd.json"
+    logging.debug(f"URL: {nzd_currency_api}")
+    json_response = read_response_json_data(nzd_currency_api)
+    nzd_conversion = json_response['nzd']
+
+    with open(local_file_name, 'w') as f:
+        json.dump(nzd_conversion, f)
+
+    bucket.upload_file(local_file_name, f'currency/{bucket_filename}')
+    logging.info(f"File uploaded to S3 to {bucket_filename}")
