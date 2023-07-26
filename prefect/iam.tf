b1ba2c6346d5e03ba58876ae98a2869c964d4ff5
Raghuram Gudimetla <gudimetla.raghu36@gmail.com>
2023-07-26 20:12:30 +1200

    Succesful prefect iac

--- prefect/iam.tf
+++ prefect/iam.tf
@@ -20,12 +20,38 @@ resource "aws_iam_policy" "prefect_user_policy" {
           "arn:aws:s3:::ap-southeast-2-886192468297-prefect/flowconfigs/",
           "arn:aws:s3:::ap-southeast-2-886192468297-prefect/flowconfigs/*"
         ]
+      },
+      {
+        Action = [
+          "ec2:DescribeSubnets",
+          "ec2:DescribeVpcs",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:GetAuthorizationToken",
+          "ecr:GetDownloadUrlForLayer",
+          "ecs:DeregisterTaskDefinition",
+          "ecs:DescribeTasks",
+          "ecs:RegisterTaskDefinition",
+          "ecs:RunTask",
+          "iam:PassRole",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:GetLogEvents",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
       }
     ]
   })
 }
 
 resource "aws_iam_user_policy_attachment" "prefect_user_policy_attachment" {
-  user = aws_iam_user.prefect_user.name
+  user       = aws_iam_user.prefect_user.name
   policy_arn = aws_iam_policy.prefect_user_policy.arn
+}
+
+resource "aws_iam_user_policy_attachment" "prefect_user_ecs_full_access" {
+  user       = aws_iam_user.prefect_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 }
